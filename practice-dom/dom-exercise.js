cc3438ce90da21b9f3a07c3cb6147d974532965d
G285152022 <228515@st.takushoku-u.ac.jp>
2023-06-29 18:02:45 +0900

    第4回演習

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,7 +14,11 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-
+let h2 = document.querySelector('h2#ex42');
+let p = document.createElement('p');
+p.style.textEmphasis='sesame green';
+p.textContent = '写真表と都市の緯度軽度のページです'
+h2.insertAdjacentElement('afterend', p);
 
 // 練習4-3 写真表作成プログラム
 
