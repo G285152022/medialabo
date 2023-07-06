643a35062133d5da9f1c21d05ca2ac5cb71c9bdf
G285152022 <228515@st.takushoku-u.ac.jp>
2023-07-06 17:21:23 +0900

    数当てゲーム

--- kazuate.js
+++ kazuate.js
@@ -9,12 +9,18 @@ let kaisu = 1;
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 let b = document.querySelector('#print');
-b.addEventListener('click', greeting);
+b.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="seisu"]');
+  let yoso = i.value;
   
+  let button = document.querySelector('button#print');
+  let p = document.createElement('p');
+  p.textContent = (kaisu + '回目の予想: ' + yoso);
+  button.insertAdjacentElement('afterend', p);
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
